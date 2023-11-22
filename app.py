492a88213e33c980c5279bc8e161a46ccc861e20
( DO DREAM DEVELOPMENT) DEVELOPER D 실행하고 꿈꾸고 발전하는개발자.. 가 되고싶습니다 <68416184+silver-liq9118@users.noreply.github.com>
2023-11-22 15:41:14 +0900

    main update

--- app.py
+++ app.py
@@ -1,55 +1,74 @@
-import requests
-import json
-import sys
 import time
+import slack_messenger
+import sys
+import datetime
+sys.path.append("crawler")
 
-class SlackMessenger:
-    def __init__(self, webhook_url):
-        self.webhook_url = webhook_url
+from crawler.darkweb import Crawler
 
-    def _get_current_time(self):
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+class Main:
+    def __init__(self):
+        self.crawler = Crawler()
+        self.messenger = slack_messenger.SlackMessenger()
+        self.start_time = datetime.datetime.now()
 
-    def send_message(self, msg):
-        timestamp = self._get_current_time()
-        message = f"{timestamp}: {msg}"
-        title = ":zap: Leak-Information-Notification :zap:"
+    def print_welcome(self):
+        welcome_message = r"""
+   __        __       ___ ___     ____       __     
+ /'_ `\    /'__`\   /' __` __`\  /\_ ,`\   /'__`\   
+/\ \L\ \  /\ \L\.\_ /\ \/\ \/\ \ \/_/  /_ /\ \L\.\_ 
+\ \____ \ \ \__/.\_\\ \_\ \_\ \_\  /\____\\ \__/.\_\
+ \/___L\ \ \/__/\/_/ \/_/\/_/\/_/  \/____/ \/__/\/_/
+   /\____/                                          
+   \_/__/ 
+__                          __              __              __                
+/\ \                       /\ \            /\ \            /\ \__             
+\ \ \         __      __   \ \ \/'\        \_\ \      __   \ \ ,_\     __     
+ \ \ \  __  /'__`\  /'__`\  \ \ , <        /'_` \   /'__`\  \ \ \/   /'__`\   
+  \ \ \L\ \/\  __/ /\ \L\.\_ \ \ \\`\     /\ \L\ \ /\ \L\.\_ \ \ \_ /\ \L\.\_ 
+   \ \____/\ \____\\ \__/.\_\ \ \_\ \_\   \ \___,_\\ \__/.\_\ \ \__\\ \__/.\_\
+    \/___/  \/____/ \/__/\/_/  \/_/\/_/    \/__,_ / \/__/\/_/  \/__/ \/__/\/_/
+                                                                                             
+        """
+        print(welcome_message)  
 
-        slack_data = {
-            "username": "leak-info-notification",
-            "icon_emoji": ":satellite:",
-            "channel": "#leak-info-notification",
-            "attachments": [
-                {
-                    "color": "#9733EE",
-                    "fields": [
-                        {
-                            "title": title,
-                            "value": message,
-                            "short": "true",
-                        }
-                    ]
-                }
-            ]
-        }
+    def crawler_start(self):
+        alarm_data = self.crawler.start()
+        return alarm_data
 
-        byte_length = str(sys.getsizeof(slack_data))
-        headers = {'Content-Type': "application/json", 'Content-Length': byte_length}
-        
-        response = requests.post(self.webhook_url, data=json.dumps(slack_data), headers=headers)
-        
-        if response.status_code != 200:
-            raise Exception(response.status_code, response.text)
-
-def main():
-    webhook_url = "https://hooks.slack.com/services/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"
-    messenger = SlackMessenger(webhook_url)
-
-    # Example message
-    message = "Test!"
-
-    # Send the message
-    messenger.send_message(message)
+    def send_message(self, message):
+        self.messenger.send_message(message)
 
 if __name__ == "__main__":
-    main()
+    Main = Main()
+    Main.print_welcome()
+    print("---------------------------------------------------------")
+    print(f"Program started at: {Main.start_time}")
+    print("---------------------------------------------------------")
+
+    while True:
+        alarm_data = Main.crawler_start()
+
+        if alarm_data:
+            alarm_data = alarm_data[0]
+
+            current_time = datetime.datetime.now()
+            elapsed_time = current_time - Main.start_time
+
+            slack_message = f"Domain: {alarm_data['domain']}\n" \
+                            f"URL: {alarm_data['url']}\n" \
+                            f"Status: {alarm_data['status']}\n" \
+                            f"Deadline: {alarm_data['deadline']}\n" \
+                            f"Description: {alarm_data['description']}\n" \
+                            f"Uploaded Date: {alarm_data['uploaded_date']}\n" \
+                            f"Updated Date: {alarm_data['updated_date']}\n" \
+                            f"Start Time: {Main.start_time}\n" \
+                            f"Elapsed Time: {elapsed_time}"
+
+            Main.send_message(slack_message)
+        else:
+            current_time = datetime.datetime.now()
+            elapsed_time = current_time - Main.start_time
+            print(f"No alarm data found at: {current_time}, Elapsed Time: {elapsed_time}")
+
+        time.sleep(10)
---
+++ slack_messenger.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import sys
+import time
+
+class SlackMessenger:
+    def __init__(self):
+        self.webhook_url = "https://hooks.slack.com/services/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"
+
+    def _get_current_time(self):
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    def send_message(self, msg):
+        timestamp = self._get_current_time()
+        message = f"{timestamp}: {msg}"
+        title = ":zap: Leak-Information-Notification :zap:"
+
+        slack_data = {
+            "username": "leak-info-notification",
+            "icon_emoji": ":satellite:",
+            "channel": "#leak-info-notification",
+            "attachments": [
+                {
+                    "color": "#9733EE",
+                    "fields": [
+                        {
+                            "title": title,
+                            "value": message,
+                            "short": "true",
+                        }
+                    ]
+                }
+            ]
+        }
+
+        byte_length = str(sys.getsizeof(slack_data))
+        headers = {'Content-Type': "application/json", 'Content-Length': byte_length}
+        
+        response = requests.post(self.webhook_url, data=json.dumps(slack_data), headers=headers)
+        
+        if response.status_code != 200:
+            raise Exception(response.status_code, response.text)
+
+def main():
+    messenger = SlackMessenger()
+
+    message = "Test!"
+
+    messenger.send_message(message)
+
+if __name__ == "__main__":
+    main()
